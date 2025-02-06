328c872640da9b63751fb365fa5d4d0f2d4d694a
Rishi Khare <rishiskhare@gmail.com>
2025-02-06 04:26:24 -0800

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
 import type React from "react"
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,7 +21,10 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="icon" href="https://en.wikipedia.org/static/favicon/wikipedia.ico" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
